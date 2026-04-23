72806df2c480af8d9a0bb5ce315af67ba4e8c713
Kunal Bhavasar <102582086+KunalB96@users.noreply.github.com>
2026-04-23 14:58:34 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['html']],
   
+  timeout: 60000,
+  retries: 2,
+  workers: 2,
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -33,6 +37,10 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     headless: false,
     trace: 'on-first-retry',
+    
+    headless: true,
+    actionTimeout: 15000,
+    navigationTimeout: 30000,
   },
 
   /* Configure projects for major browsers */
